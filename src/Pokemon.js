cd30452fc57d764a679c60b4052d2b1a8e8d1981
blenderous <hpone.n.only@gmail.com>
2019-04-16 20:33:02 +0530

    Added pokedex.json

---
+++ src/Pokemon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Pokemon extends React.Component {
+    render() {
+        return (
+            <div className="pokemon-container">
+                <p className="pokemon-name">
+                    Pokemon name comes here.
+                </p>
+            </div>
+        )
+    }
+}
