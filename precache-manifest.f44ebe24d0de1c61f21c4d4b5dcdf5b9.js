2589f60eea3072f38a47e292428aa179b2fcf03c
blenderous <hpone.n.only@gmail.com>
2019-06-05 14:14:30 +0530

    Updates

---
+++ precache-manifest.f44ebe24d0de1c61f21c4d4b5dcdf5b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "dff5db81241219944e70",
+    "url": "/pokedex/static/js/main.69008646.chunk.js"
+  },
+  {
+    "revision": "5adf834b0f8db29f047e",
+    "url": "/pokedex/static/js/2.9853bcd8.chunk.js"
+  },
+  {
+    "revision": "dff5db81241219944e70",
+    "url": "/pokedex/static/css/main.02c85860.chunk.css"
+  },
+  {
+    "revision": "5adf834b0f8db29f047e",
+    "url": "/pokedex/static/css/2.f3accf2b.chunk.css"
+  },
+  {
+    "revision": "5be512aaedc287f71bc0c8c8ee0cb2d4",
+    "url": "/pokedex/index.html"
+  }
+];
