85b9c7647f6fc99c3bdb2f53179f63a19e685ff6
Harish <hp.onenonly@gmail.com>
2021-02-25 19:41:27 +0530

    Updates

---
+++ precache-manifest.d576257eac0ab96e4053e7d5fdad57e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "4c775815ae0e9c3e8981",
+    "url": "/pokedex/static/js/main.163c9d7d.chunk.js"
+  },
+  {
+    "revision": "940dc194eb48ebc8297d",
+    "url": "/pokedex/static/js/2.0557378b.chunk.js"
+  },
+  {
+    "revision": "4c775815ae0e9c3e8981",
+    "url": "/pokedex/static/css/main.cd4215f1.chunk.css"
+  },
+  {
+    "revision": "940dc194eb48ebc8297d",
+    "url": "/pokedex/static/css/2.5e8014e3.chunk.css"
+  },
+  {
+    "revision": "6b3cd49ae3ed59e20b2078c7724fc58d",
+    "url": "/pokedex/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pokedex/precache-manifest.a04831dbf353cad6f66e5c983e1363ca.js"
+  "/pokedex/precache-manifest.d576257eac0ab96e4053e7d5fdad57e5.js"
 );
 
 workbox.clientsClaim();
